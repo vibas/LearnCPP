05355866b2939e20ae65496e92b16757feef64a7
vibas.behera <vibas.behera095@gmail.com>
2023-05-07 23:29:45 +0530

    Inheritance

--- ConsoleApplication1/OOPS.cpp
+++ ConsoleApplication1/OOPS.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 #include "OOPS.h"
 
+#pragma region ClassObjectBasics
+// ============== Class / Objects / Constructor / Access Specifier ===========
 class StudentClass
 {
+	int Age; // Private by default
 private:
 	int StudentID;
 	string StudentName;
@@ -12,10 +15,17 @@ private:
 public:
 	StudentClass(int studentID, string studentName);
 	void SetStudentAddress(string address);
+
+	void SetStudentAge(int age)
+	{
+		Age = age;
+	}
+
 	void PrintStudentInfo()
 	{
 		cout << "Student ID = " << StudentID << endl;
 		cout << "Student Name = " << StudentName << endl;
+		cout << "Student Age = " << Age << endl;
 		cout << "Student Address = " << StudentAddress << endl;
 	}
 };
@@ -30,11 +40,79 @@ void StudentClass::SetStudentAddress(string address)
 {
 	StudentAddress = address;
 }
+// ============================================================================
+#pragma endregion
+
+#pragma region Inheritance
+// ========== Inheritance ======================
+class Employee
+{
+public:
+	void SetEmployeeDetails(int id, string name, string address)
+	{
+		ID = id;
+		Name = name;
+		Address = address;
+	}
+
+	void SetSalary(int salary)
+	{
+		Salary = salary;
+	}
+
+	int GetSalary()
+	{
+		return Salary;
+	}
+
+	void PrintEmployeeDetails()
+	{
+		cout << "Employee Details " << endl;
+		cout << "ID : " << ID << " Name : " << Name << " Address : " << Address << endl;
+	}
+
+private:
+	int ID;
+	string Name;
+	string Address;
+	int Salary;
+};
+
+class PermanentEmployee : public Employee
+{
+public:
+	void SetBonus(int bonus)
+	{
+		Bonus = bonus;
+	}
+
+	int GetSalary()
+	{
+		int baseSalary = Employee :: GetSalary();
+		return baseSalary + Bonus;
+	}
+
+private:
+	int Bonus;
+};
+
+class ContractualEmployee : public Employee
+{
+public:
+
+
+private:
+
+};
+
+// =============================================
+#pragma endregion
 
 void LearnOOPS()
 {
 	// Class Object Test
-	ClassObjectTest();
+	//ClassObjectTest();
+	InheritanceTest();
 }
 
 void ClassObjectTest()
@@ -44,11 +122,30 @@ void ClassObjectTest()
 	/*student1.StudentID = 1;
 	student1.StudentName = "Vibas";*/
 	student1.SetStudentAddress("Pune");
+	student1.SetStudentAge(33);
 
 	/*student2.StudentID = 2;
 	student2.StudentName = "Pinky";*/
 	student2.SetStudentAddress("Mumbai");
+	student2.SetStudentAge(23);
 
 	student1.PrintStudentInfo();
 	student2.PrintStudentInfo();
+}
+
+void InheritanceTest()
+{
+	PermanentEmployee permanent_employee;
+	ContractualEmployee contractual_employee;
+
+	permanent_employee.SetEmployeeDetails(1, "Vibas", "Pune");
+	permanent_employee.SetSalary(10000);
+	permanent_employee.SetBonus(500);
+	permanent_employee.PrintEmployeeDetails();
+	cout << "Total Salary = " << permanent_employee.GetSalary() << endl;
+
+	contractual_employee.SetEmployeeDetails(2, "Pinky", "Mumbai");
+	contractual_employee.SetSalary(5000);
+	contractual_employee.PrintEmployeeDetails();
+	cout << "Total Salary = " << contractual_employee.GetSalary() << endl;
 }
--- ConsoleApplication1/OOPS.h
+++ ConsoleApplication1/OOPS.h
@@ -2,3 +2,4 @@
 
 void LearnOOPS();
 void ClassObjectTest();
+void InheritanceTest();
